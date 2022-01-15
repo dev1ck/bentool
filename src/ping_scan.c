a778636ba8a5699a053db98f8e34505f14cd5b2c
dev1ck <devick99@gmail.com>
2022-01-15 23:55:21 +0900

    update ping_scan.c/.h files

--- src/ping_scan.c
+++ src/ping_scan.c
@@ -1,6 +1,36 @@
 #include "ping_scan.h"
 
-void ping_scan(char* IP_addr) 
+int main(int argc, char *argv[]) 
 {
+    char ipaddr[IPv4_BUFFER], prefix[PREFIX_BUFFER], *ptr;
+    struct sockaddr_in dest_addr, src_addr;
+    int sock;
+    struct icmphdr *icmp;
 
+    if(!(ptr=strchr(argv[1],'/')))
+    {
+        strcpy(ipaddr, argv[1]);
+        if((sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP))<0)
+        {
+           exit(1);
+        }
+        memset(&dest_addr, 0 ,sizeof(dest_addr));
+        inet_aton(ipaddr, &dest_addr.sin_addr);
+        memset(&icmp,0,sizeof(icmp));
+        icmp->type = ICMP_ECHO;
+        icmp->checksum = icmp_cksum();
+    }
+    else
+    {
+        strtok(argv[1],"/");
+        strcpy(ipaddr,argv[1]);
+        strcpy(prefix, ptr+1);
+        if(atoi(prefix)>32 || atoi(prefix) < 0)
+        {
+            exit(1);
+        }
+
+
+    }
+  
 }
--- src/ping_scan.h
+++ src/ping_scan.h
@@ -1,7 +1,13 @@
 #pragma once
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <netinet/ip_icmp.h>
+
+#define IPv4_BUFFER 20
+#define PREFIX_BUFFER 5
 
 void ping_scan(char *IP_addr);
