c8f489a162295810a56cec97267b16b03e6b8835
haekt <haekt1140@gmail.com>
2022-01-17 19:09:49 +0900

    daily_log.md

--- haekt/studycodes/arp.c
+++ haekt/studycodes/arp.c
@@ -1,44 +1,44 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> // 기본 입출력 라이브러리
+#include <string.h> // 문자열을 다루기 위한 라이브러리 
 
-#ifndef __linux__
-	#include <winsock2.h>
-#else
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
-#endif
+//#ifndef __linux__
+//	#include <winsock2.h> // 윈도우 소켓 라이브러리
+//#else
+	#include <netinet/in.h> // IPv4 전용 기능 라이브러리
+	#include <arpa/inet.h> // 주소 조작,변환 라이브러리 
+//#endif
 
-#include "arp.h"
+#include "./arp.h" // arp.h 파일을 포함 
 
 int main(void)
 {
-	struct ether_arp ether_arp;
-	int src_ip, dst_ip;
-	int i;
+	struct ether_arp ether_arp; // mac주소, 프로토콜 주소, 길이 를 포함하는 구조체 변수 ether_arp 선언 
+	int src_ip, dst_ip; // 소스 ip 변수 , 목적지 ip 변수 선언 
+	int i; //반복을 위한 i 변수 선언 
 
-	ether_arp.arp_hrd = htons(1);
-	ether_arp.arp_pro = htons(0x0800);
-	ether_arp.arp_hln = 6;
-	ether_arp.arp_pln = 4;
-	ether_arp.arp_op = htons(ARPOP_REQUEST);
+	ether_arp.arp_hrd = htons(1); // 1 (이더넷 ) int 형 데이터를 네트워크 순서로 변환해 mac 주소에 저장 
+	ether_arp.arp_pro = htons(0x0800); // 0x0800(ipv4) int 형 데이터를 네트워크 순서로 변환해 protocol 주소에 저장
+	ether_arp.arp_hln = 6; // mac 주소의 크기를 6byte 로 설정 
+	ether_arp.arp_pln = 4; // protocol 주소의 크기를 4byte 로 설정 
+	ether_arp.arp_op = htons(ARPOP_REQUEST); // arp.h 안의 arp request 값인 1 을 네트워크 순서로 변환해 arp_op에 저장 ( 0001 은 request , 0002 는 reply)
 
-	src_ip = inet_addr("1.2.3.4");
-	dst_ip = inet_addr("5.6.7.8");
+	src_ip = inet_addr("1.2.3.4"); // 문자형 샌더 주소 정수형으로 src_ip 에 저장 
+	dst_ip = inet_addr("5.6.7.8"); // 문자형 목적지 주소를 정수형으로 dst_ip 에 저장 
 
-	memcpy(ether_arp.arp_spa, &src_ip, 4);
-	memcpy(ether_arp.arp_tpa, &dst_ip, 4);
+	memcpy(ether_arp.arp_spa, &src_ip, 4); // src_ip 의 프로토콜 주소를 4byte 크기만큼 arp_spa 에 저장 
+	memcpy(ether_arp.arp_tpa, &dst_ip, 4); // dst_ip 의 목적지 프로토콜 주소를 4byte 크기만큼 arp_tpa 에 저장
 
-	for(i = 0; i < 6; i += 1) {
-		ether_arp.arp_sha[i] = i;
+	for(i = 0; i < 6; i += 1) { // 6번 반복 
+		ether_arp.arp_sha[i] = i; // sender mac 주소를 0~6 까지 저장 -> 1,2,3,4,5,6
 	}
 
 	for(i = 0; i < 6; i += 1) {
-		ether_arp.arp_tha[i] = i + 0x10;
+		ether_arp.arp_tha[i] = i + 0x10; // target mac 주소를 0~6 까지 저장 
 	}
 
-	printf("ether_arp : %lu\n", (unsigned long)sizeof(struct ether_arp));
+	printf("ether_arp : %lu\n", (unsigned long)sizeof(struct ether_arp)); // ether_arp구조체의 크기를 출력
 
-	dump_arp_header(&ether_arp);
+	dump_arp_header(&ether_arp); // ether_arp 구조체 변수 arp 에 ether_arp의 내용을 저장 
 
 	return 0;
 }
--- haekt/studycodes/arp.h
+++ haekt/studycodes/arp.h
@@ -1,33 +1,34 @@
-#ifndef ARP_H
+#ifndef ARP_H // arp 헤더가 정의 되지 않았을 경우 arp 헤더를 정의해줌 
 #define ARP_H
 
-#include <stdint.h>
+#include <stdint.h> // 자료형 정의 헤더 파일.
 
-#define	ARPOP_REQUEST	1		/* ARP request.  */
-#define	ARPOP_REPLY	2		/* ARP reply.  */
+#define	ARPOP_REQUEST 1		/* ARP request.  */
+#define	ARPOP_REPLY	2		/* ARP reply.  */ 
+// ( 0001 은 request , 0002 는 reply)
 
-#ifndef __linux__
-	#pragma pack(push, 1)
+#ifndef __linux__  // 리눅스가 아닐 경우 
+	#pragma pack(push, 1) // 구조체의 기준 바이트를 1 로 만든다. -> 빈공간 없이 차곡 차곡 저장한다.
 #endif
-struct arphdr
+struct arphdr // arp 헤더의 type과 주소의 길이, operatrion 을 저장하는 구조체
 {
 	uint16_t ar_hrd;		/* Format of hardware address.  */
 	uint16_t ar_pro;		/* Format of protocol address.  */
 	uint8_t ar_hln;		/* Length of hardware address.  */
 	uint8_t ar_pln;		/* Length of protocol address.  */
-	uint16_t ar_op;		/* ARP opcode (command).  */
+	uint16_t ar_op;		/* ARP operatrion code (command).  */
 }
 #ifndef __linux__
 	;
-	#pragma pack(pop)
+	#pragma pack(pop)// 구조체 기준 1바이트 해제. 차곡차곡 저장하던걸 기본으로 돌려놓음 
 #else
-__attribute__ ((__packed__));
+__attribute__ ((__packed__)); // 구조체 바이트 빈공간 제어 -> 빈공간 없이 차곡차곡 저장 
 #endif
 
 #ifndef __linux__
-	#pragma pack(push, 1)
+	#pragma pack(push, 1) // 구조체 기준 바이트 1 로 설정
 #endif
-struct	ether_arp 
+struct	ether_arp // 샌더와 목적지 arp 헤더 주소를 저장하는 구조체
 {
 	struct	arphdr ea_hdr;		/* fixed-size header */
 
@@ -38,17 +39,17 @@ struct	ether_arp
 }
 #ifndef __linux__
 	;
-	#pragma pack(pop)
+	#pragma pack(pop) // 구조체 기준 1바이트 해제
 #else
-__attribute__ ((__packed__));
+__attribute__ ((__packed__)); // 구조체 바이트 빈공간 제어 -> 빈공간 없이 차곡차곡 저장
 #endif
 
-#define	arp_hrd	ea_hdr.ar_hrd
-#define	arp_pro	ea_hdr.ar_pro
-#define	arp_hln	ea_hdr.ar_hln
-#define	arp_pln	ea_hdr.ar_pln
-#define	arp_op	ea_hdr.ar_op
+#define	arp_hrd	ea_hdr.ar_hrd // 구조체 변수 arp_hrd 선언 - 맥주소 
+#define	arp_pro	ea_hdr.ar_pro // 구조체 변수 arp_pro 선언 - 프로토콜 주소 
+#define	arp_hln	ea_hdr.ar_hln // 구조체 변수 arp_hln 선언 - 맥주소 길이 
+#define	arp_pln	ea_hdr.ar_pln // 구조체 변수 arp_pln 선언 - 프로토콜 주소길이
+#define	arp_op	ea_hdr.ar_op // 구조체 변수 arp_op 선언 - 오퍼레이트, 요청 패킷인지 응답 패킷인지 결정
 
-void dump_arp_header(struct ether_arp *arp);
+void dump_arp_header(struct ether_arp *arp); // 목적지와 샌더의 주소를 갖는 구조체 변수 arp 를 선언하는 함수 dump_arp_header 선언
 
 #endif
