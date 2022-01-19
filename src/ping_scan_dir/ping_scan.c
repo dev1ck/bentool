77f70b1bc43eb367e64afe291a7c8a6bc5dcd8e6
dev1ck <devick99@gmail.com>
2022-01-19 11:21:53 -0500

    update ping_scan.c

--- src/ping_scan_dir/ping_scan.c
+++ src/ping_scan_dir/ping_scan.c
@@ -2,14 +2,14 @@
 
 int main(int argc, char *argv[]) 
 {
-    char *ptr, buf[32]; 
+    char *ptr;
     struct sockaddr_in reply_addr;
     struct icmp_packet icmp_p;
     int sock, prefix;
     u_int32_t start_ip, end_ip, ip, mask;
-
     socklen_t addr_size = sizeof(reply_addr);
     memset(&reply_addr, 0, sizeof(reply_addr));
+    pthread_t thread_id;
 
     if((sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP))<0)
     {
@@ -23,14 +23,14 @@ int main(int argc, char *argv[])
     icmp_p.icmp.icmp_id = 1;
     icmp_p.icmp.icmp_seq = 1;
     icmp_p.icmp.icmp_cksum = cksum((unsigned short*)&icmp_p, sizeof(icmp_p));
-    //icmp_p.icmp.un.echo.id = 1;
-    //icmp_p.icmp.un.echo.sequence = 1;
-    
+
+    // pthread_create(&thread_id, NULL, thread_function, &sock);
     if(!(ptr=strchr(argv[1],'/')))
     {
         ip = ntohl(inet_addr(argv[1]));
         if(send_ping(sock, ip, icmp_p)<0)
             printf("error ping\n");
+        receive_ping(sock);
     }
     else
     {
@@ -51,17 +51,9 @@ int main(int argc, char *argv[])
         {
             if(send_ping(sock, ip, icmp_p)<0)
                 printf("error ping\n");
+            receive_ping(sock);
         }
     }
-    
-    if((recvfrom(sock, buf, sizeof(buf),0,(struct sockaddr*)&reply_addr, &addr_size))<=0)
-        printf("fail_recive ping\n");
-    else
-    {
-        printf("%s\n",inet_ntoa(reply_addr.sin_addr));
-        printf("%s\n",buf);
-    }
-    
     close(sock);
 
     return 0;
@@ -101,7 +93,48 @@ int send_ping(int sock, u_int32_t ip, struct icmp_packet icmp_p)
 
     if(sendto(sock, &icmp_p, sizeof(icmp_p), 0, (struct sockaddr *)&addr, sizeof(addr)) < 0) return -1;
     DDN_ip = inet_ntoa(addr.sin_addr);
-    printf("send ping : %s\n", DDN_ip);
+    //printf("send ping : %s\n", DDN_ip);
 
     return 1;
+}
+
+// void *thread_function(void *p)
+// {
+// 	char buffer[BUFMAX];
+// 	int sock, len; 
+	
+// 	sock= *((int *)p);
+
+// 	while((len = read(sock, buffer, BUFMAX)) > 0) {
+// 		struct ip *ip = (struct ip *)buffer;
+// 		int ip_header_len = ip->ip_hl << 2;
+
+// 		if(ip->ip_p == IPPROTO_ICMP) {
+// 			struct icmp *icmp = (struct icmp *)(buffer + ip_header_len);
+// 			if((icmp->icmp_type == 0) && (icmp->icmp_code == 0)) {
+// 				printf("from : %s\n", inet_ntoa(ip->ip_src));
+// 			}
+// 		}
+// 	}
+
+// 	return 0;
+// }
+
+void receive_ping(int sock)
+{
+    char buffer[BUFMAX];
+    int len;
+
+    if((len = read(sock, buffer ,BUFMAX))>0)
+    {
+        struct ip *ip = (struct ip *)buffer;
+		int ip_header_len = ip->ip_hl << 2;
+
+		if(ip->ip_p == IPPROTO_ICMP) {
+			struct icmp *icmp = (struct icmp *)(buffer + ip_header_len);
+			if((icmp->icmp_type == 0) && (icmp->icmp_code == 0)) {
+				printf("from : %s\n", inet_ntoa(ip->ip_src));
+			}
+		}
+    }
 }
--- src/ping_scan_dir/ping_scan.h
+++ src/ping_scan_dir/ping_scan.h
@@ -7,10 +7,27 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <pthread.h>
 
-#define IPv4_BUFFER 20
-#define PREFIX_BUFFER 5
+#define BUFMAX 4096
 
+struct ip
+{
+	uint8_t ip_hl:4;		/* header length */
+	uint8_t ip_v:4;		/* version */
+	uint8_t ip_tos;			/* type of service */
+	uint16_t ip_len;			/* total length */
+	uint16_t ip_id;			/* identification */
+	uint16_t ip_off;			/* fragment offset field */
+#define	IP_RF 0x8000			/* reserved fragment flag */
+#define	IP_DF 0x4000			/* dont fragment flag */
+#define	IP_MF 0x2000			/* more fragments flag */
+#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
+	uint8_t ip_ttl;			/* time to live */
+	uint8_t ip_p;			/* protocol */
+	uint16_t ip_sum;			/* checksum */
+	struct in_addr ip_src, ip_dst;	/* source and dest address */
+};
 struct icmp
 {
 	uint8_t  icmp_type;
@@ -28,4 +45,6 @@ struct icmp_packet
 void ping_scan(char *IP_addr);
 uint16_t cksum(uint16_t *data, uint32_t len);
 int send_ping(int sock, u_int32_t ip, struct icmp_packet icmp_packet);
+void *thread_function(void *p);
+void receive_ping(int sock);
 
